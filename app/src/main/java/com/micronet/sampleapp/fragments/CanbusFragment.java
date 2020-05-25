13dd50c511e3a8a05cd79517c801518a0050ac2f
vladimirz <vladimir.zatulovsky@micronet-inc.com>
2020-05-25 11:06:23 +0300

    Version 1.8.0 Close CAN when tabs changed view

--- app/src/main/java/com/micronet/sampleapp/fragments/CanbusFragment.java
+++ app/src/main/java/com/micronet/sampleapp/fragments/CanbusFragment.java
@@ -98,6 +98,12 @@ public class CanbusFragment extends Fragment implements OnClickListener, Adapter
     }
 
     @Override
+    public void onPause() {
+        closeCanbus();
+        super.onPause();
+    }
+
+    @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         rootView = inflater.inflate(R.layout.fragment_canbus, container, false);
